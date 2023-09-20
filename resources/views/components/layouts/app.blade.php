3f321e69fd71e2254f22e45ca1a08d9cead22bbc
Mohammed Hamad <toshiba9895@gmail.com>
2023-09-21 03:14:27 +0400

    fix floating just for home

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -22,42 +22,6 @@
     @livewireScripts
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/floating.js/2.6.2/floating.js" integrity="sha512-n5IYFs4Ln9LoU6l75MoZTy7NpnqEo2PmYxJoiQOmPpsk/epcOX2ozdTJ9mEV29spCJTugzsH8mOx2nkVQ8cZdg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="{{ asset('js/main.js') }}"></script>
-    <script>
-        floating({
-            content: "<img src='{{asset('images/main.svg')}}' />",
-            number: 3,
-            direction: 'normal',
-            duration: 11,
-            repeat: 1,
-            size: 5,
-        });
-        floating({
-            content: "<img src='{{asset('images/main2.svg')}}' />",
-            number: 5,
-            direction: 'normal',
-            duration: 8,
-            repeat: 1,
-            size: 6,
-        });
-        floating({
-            content: "<img src='{{asset('images/main3.svg')}}' />",
-            number: 3,
-            direction: 'normal',
-            duration: 15,
-            repeat: 1,
-            size: 7,
-        });
-        floating({
-            content: "<img src='{{asset('images/main4.svg')}}' />",
-            number: 3,
-            repeat: 1,
-            direction: 'normal',
-            duration: 3,
-            repeat: 1,
-            size: 8,
-        });
-    </script>
 </body>
 </html>
--- resources/views/livewire/frontend/home.blade.php
+++ resources/views/livewire/frontend/home.blade.php
@@ -180,5 +180,40 @@
             </div>
         </div>
     </section>
-
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/floating.js/2.6.2/floating.js" integrity="sha512-n5IYFs4Ln9LoU6l75MoZTy7NpnqEo2PmYxJoiQOmPpsk/epcOX2ozdTJ9mEV29spCJTugzsH8mOx2nkVQ8cZdg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script>
+        floating({
+            content: "<img src='{{asset('images/main.svg')}}' />",
+            number: 3,
+            direction: 'normal',
+            duration: 11,
+            repeat: 1,
+            size: 5,
+        });
+        floating({
+            content: "<img src='{{asset('images/main2.svg')}}' />",
+            number: 5,
+            direction: 'normal',
+            duration: 8,
+            repeat: 1,
+            size: 6,
+        });
+        floating({
+            content: "<img src='{{asset('images/main3.svg')}}' />",
+            number: 3,
+            direction: 'normal',
+            duration: 15,
+            repeat: 1,
+            size: 7,
+        });
+        floating({
+            content: "<img src='{{asset('images/main4.svg')}}' />",
+            number: 3,
+            repeat: 1,
+            direction: 'normal',
+            duration: 3,
+            repeat: 1,
+            size: 8,
+        });
+    </script>
 </div>
